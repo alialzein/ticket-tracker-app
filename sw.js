84823523ed424be46196b46bc066c69d1d9b0fa6
alialzein <alizein1997@gmail.com>
2026-02-25 10:37:22 +0200

    new layout

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // TeamsOps Service Worker
 // Cache version — bump this string when deploying new code to invalidate old caches
-const CACHE_VERSION = 'teamsops-v33';
+const CACHE_VERSION = 'teamsops-v34';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
 // App shell files to pre-cache on install
